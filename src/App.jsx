cfb7b84188ef2125cd141bb19fd2a07089829e5d
Marco Navarrete <marconav2000@gmail.com>
2024-08-18 18:26:25 -0400

    eliminación de linea no uasa en archivo app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,6 @@ import {BrowserRouter, Route, Routes} from "react-router-dom"
 
 
 
-
 const App = () => {
 
  
@@ -21,8 +20,7 @@ const App = () => {
       <BrowserRouter>
 
         <Navbar />
-        {/* <Home /> */}
-
+    
         <Routes>
 
           <Route path="/" element={<Home />} />
